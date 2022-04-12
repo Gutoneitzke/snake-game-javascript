c7318ed89861b23334666eb22380aeb40134b9e6
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-04-11 22:08:31 -0300

    our v.1 script

---
+++ game.js
@@ -0,0 +1,17 @@
+const SNAKE_SPEED = 5;
+
+setInterval(main,SNAKE_SPEED*100)
+
+function main() {
+    console.log('MAIN');
+}
+
+// Loop -> Algo que vai se repetir para efetuar uma renderização
+function update() {
+    // atualiza a comida e a cobra
+}
+
+// Draw/Render
+function render() {
+    // atualiza a posição
+}
